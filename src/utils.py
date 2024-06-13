65bb1da4e9772309924e8cc0015ff82f2c4a4dca
Albert <autelec@yandex.ru>
2024-06-14 02:30:26 +0400

    Создал функцию get_amount

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 import os
 import json
-import requests
+
+from src.external_api import get_convert
+
 
 def load_transactions(file_path: str) -> list:
     """которая принимает на вход путь до JSON-файла и возвращает список словарей с данными о
@@ -8,13 +10,13 @@ def load_transactions(file_path: str) -> list:
     Если файл пустой, содержит не список или не найден, функция возвращает пустой список"""
 
     base_path = os.path.dirname(__file__)
-    full_path = os.path.join(base_path, '..', file_path)
+    full_path = os.path.join(base_path, "..", file_path)
 
     try:
         if not os.path.exists(full_path):
             return []
 
-        with open(full_path, 'r', encoding='utf-8') as file:
+        with open(full_path, "r", encoding="utf-8") as file:
             data = json.load(file)
 
             if isinstance(data, list):
@@ -25,29 +27,31 @@ def load_transactions(file_path: str) -> list:
     except (json.JSONDecodeError, IOError):
         return []
 
+
 def get_amount(transaction: dict) -> float:
     """Функция принимает на вход транзакцию и возвращает сумму транзакции в рублях"""
-    return transaction['operationAmount']['amount']
+    code_ = transaction["operationAmount"]["currency"]["code"]
+    amount_ = transaction["operationAmount"]["amount"]
+    if code_ in ("USD", "EUR"):
+        return get_convert(code_, amount_)
+    else:
+        return amount_
 
 
-if __name__ == '__main__':
-    transactions = load_transactions('data/operations.json')
+if __name__ == "__main__":
+    transactions = load_transactions("data/operations.json")
     print(transactions)
 
-    amount = get_amount({
-    "id": 441945886,
-    "state": "EXECUTED",
-    "date": "2019-08-26T10:50:58.294041",
-    "operationAmount": {
-      "amount": "31957.58",
-      "currency": {
-        "name": "руб.",
-        "code": "RUB"
-      }
-    },
-    "description": "Перевод организации",
-    "from": "Maestro 1596837868705199",
-    "to": "Счет 64686473678894779589"
-  })
+    amount = get_amount(
+        {
+            "id": 522357576,
+            "state": "EXECUTED",
+            "date": "2019-07-12T20:41:47.882230",
+            "operationAmount": {"amount": "51463.70", "currency": {"name": "USD", "code": "USD"}},
+            "description": "Перевод организации",
+            "from": "Счет 48894435694657014368",
+            "to": "Счет 38976430693692818358",
+        }
+    )
 
     print(amount)
